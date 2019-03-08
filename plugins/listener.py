48267be28dea5407208e5fa768f32ab63d4b322d
genesis-root <lionmonkey2@gmail.com>
2019-03-08 03:52:57 +0200

    Speech recognition

--- plugins/listener.py
+++ plugins/listener.py
@@ -1,11 +1,42 @@
+import io
 import queue
+import wave
 
-from disco.bot import Plugin
+from disco.bot import Plugin, Config
 from disco.bot.command import CommandError
 from disco.voice.client import VoiceException
-import opuslib.api
-import opuslib.api.decoder
+import opuslib
+import speech_recognition as sr
 
+class PCM2WAVStream:
+    def __init__(self):
+        self.buffer = io.BytesIO()
+        self.wav = wave.open(self.buffer, 'wb')
+        self.wav.setparams((2, 2, 48000, 0, 'NONE', 'NONE'))
+
+    def write(self, pcm):
+        self.wav.writeframes(pcm)
+
+    def seek(self, n):
+        self.buffer.seek(n)
+
+    def read(self, n=None):
+        if not n:
+            return self.buffer.read()
+        else:
+            return self.buffer.read(n)
+
+    def dump(self, fname):
+        self.buffer.seek(0)
+        with open(str(fname) + ".wav", "wb") as ofile:
+            ofile.write(self.buffer.read())
+
+    def close(self):
+        self.wav.close()
+        self.buffer.close()
+
+class ListenerPluginDefaultConfig(Config):
+    wit_api_key = ""
 
 class Listener:
     def __init__(self, client):
@@ -13,14 +44,16 @@ class Listener:
         self.is_recording = False
         # TODO: prevent collisions
         self.user_ofiles = {}
-        self.dec = opuslib.api.decoder.create_state(48000, 2)
+        self.dec = opuslib.Decoder(48000, 2)
         self.wqueue = queue.Queue()
 
+@Plugin.with_config(ListenerPluginDefaultConfig)
 class ListenerPlugin(Plugin):
-
     def load(self, ctx):
         super(ListenerPlugin, self).load(ctx)
+        self.wit_api_key = self.config.get('wit_api_key')
         self.guild_listeners = {}
+        self.rec = sr.Recognizer()
 
     @Plugin.command('join')
     def on_join(self, event):
@@ -71,14 +104,31 @@ class ListenerPlugin(Plugin):
             user_id, pcm = listener.wqueue.get()
             if user_id:
                 if user_id not in listener.user_ofiles:
-                    listener.user_ofiles[user_id] = open(str(user_id) + ".raw", 'wb')
-                    print(listener.user_ofiles[user_id])
+                    # listener.user_ofiles[user_id] = open(str(user_id) + ".raw", 'wb')
+                    listener.user_ofiles[user_id] = PCM2WAVStream()
                 listener.user_ofiles[user_id].write(pcm)
-                listener.user_ofiles[user_id].flush()
+                #listener.user_ofiles[user_id].flush()
+        event.msg.reply("Stopped recording, transcribing...")
         for (uid, f) in listener.user_ofiles.items():
+            audio = None
+            f.seek(0)
+            with sr.AudioFile(f) as source:
+                audio = self.rec.record(source)  # read the entire audio file
+                if audio:
+                    text = ""
+                    try:
+                        text = self.rec.recognize_wit(audio, key=self.wit_api_key)
+                    except sr.UnknownValueError:
+                        print("WIT.ai could not understand audio")
+                    except sr.RequestError as e:
+                        print("Could not request results from WIT.ai; {0}".format(e))
+            if not text:
+                event.msg.reply("Could not tell what {} said".format(user))
+            else:
+                event.msg.reply("{} said: {}".format(user, text))
+            f.dump(uid)
             f.close()
         listener.user_ofiles = {}
-        event.msg.reply("Stopped recording.")
 
     @Plugin.command('stop')
     def on_stop(self, event):
@@ -97,5 +147,5 @@ class ListenerPlugin(Plugin):
             user = event.client.guild().get_member(event.user_id)
             print("VOICE DATA from {}, type: {}, rtp: {}, nonce: {}, len(event.data): {}".format(user, event.payload_type, event.rtp, event.nonce, len(event.data)))
             frame_size = int((48000) * len(event.data))
-            pcm = opuslib.api.decoder.decode(listener.dec, event.data, len(event.data), frame_size, 0)
+            pcm = listener.dec.decode( event.data, frame_size)
             listener.wqueue.put((event.user_id, pcm))
